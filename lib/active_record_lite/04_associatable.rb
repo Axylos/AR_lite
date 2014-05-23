72d8d0bb23e2e025a59128e1d07cf25f81fc83fe
App Academy Student <student@appacademy.io>
2014-05-23 16:07:55 -0400

    has_many

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -10,19 +10,19 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.to_s.constantize
   end
 
   def table_name
-    # ...
+    class_name.to_s.downcase + "s"
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
     belongings = {
-      foreign_key: "#{name.to_s}_id".to_sym,
-      class_name: name.singularize.camelcase.to_s,
+      foreign_key: "#{name.to_s.underscore}_id".to_sym,
+      class_name: name.to_s.singularize.camelcase.to_s,
       primary_key: :id
     }
    
@@ -33,18 +33,42 @@ end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    things_to_have = {
+      foreign_key: (self_class_name.underscore + "_id").to_sym,
+      class_name: name.to_s.singularize.camelcase.to_s,
+      primary_key: :id
+    }
+    
+    things_to_have = things_to_have.merge options
+    things_to_have.each { |k, v| self.instance_variable_set("@#{k}", v) }
   end
 end
 
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    
+    define_method(name) do
+      foreign_key = self.send(options.foreign_key)
+      class_name = options.model_class
+      
+      h = { options.primary_key =>  foreign_key}
+      found_object = class_name.where(h)
+      (found_object).first
+       
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+    define_method(name) do
+      foreign_key = self.send(options.primary_key)
+      class_name = options.model_class
+  
+      h = { options.foreign_key => foreign_key }
+      class_name.where(h)
+    end
   end
 
   def assoc_options
@@ -53,5 +77,5 @@ module Associatable
 end
 
 class SQLObject
-  # Mixin Associatable here...
+  extend Associatable
 end
